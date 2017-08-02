3a80ef3e68854cfffdc594c47d03e6a84a89086a
Justyna Rudnicka-Staniewska <jete.justyce@gmail.com>
2017-08-02 15:10:14 +0200

    add initial page scroll

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 $(function(){
+    var headerButton = $('#header-button');
     var coffeeBean = $('#coffee-bean');
-    console.log(coffeeBean);
     var beanOffset = coffeeBean.offset().top;
     console.log(beanOffset);
-    
+    var initialWindowHeight = $(window).innerHeight();
+    console.log(initialWindowHeight);
+
+    var firstArticle = $('#part-one');
+
+    headerButton.on('click', function(){
+        $('main').addClass('main-flex');
+        var initialOffset = $('main').offset().top;
+        $('html, body').animate({scrollTop: initialOffset}, 1000, function(){
+            firstArticle.find('h2').fadeIn(800, function(){
+                firstArticle.find('div').fadeIn(1000);
+            });
+        }); 
+    });
+
 });
