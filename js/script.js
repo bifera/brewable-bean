8b7e74243bb4eca99b4026902c663d2936a798ac
Justyna Rudnicka-Staniewska <jete.justyce@gmail.com>
2017-08-04 10:59:00 +0200

    update scroll animation and style

--- js/script.js
+++ js/script.js
@@ -1,65 +1,42 @@
 $(function(){
     var headerButton = $('#header-button');
     var coffeeBean = $('#coffee-bean');
+    var container = $('#container');
+    var firstSection = $('#part-one');
+    var distance = firstSection.height(); // distance to scroll
+    var sections = $('section');
+    console.log(sections.length);
+    var counter = 0;
 
 
-    // extra super powers next button
+    // extra super powers next button navigating from one section to another
     var nextButton = $('<button>').addClass('button-next').attr('id', 'button-next').text('... and then what?');
 
-    // initial animation: button on header
-    headerButton.on('click', function(){
-        $('main').addClass('main-flex');
-        var firstSection = $('#part-one');
-        var firstArticle = firstSection.find('article');
-        firstSection.show();
-        firstArticle.show();
-        var initialOffset = $('main').offset().top;
-        $('html, body').animate({scrollTop: initialOffset}, 1000, function(){
-            firstArticle.find('h2').fadeIn(1000, function(){
-                firstArticle.find('div').fadeIn(1000, function(){
-                    nextButton.appendTo(firstArticle).fadeIn(1000);
-                    var offsetTopForBean = $(window).height()/2;
-                    var offsetLeftForBean = $(window).width()/2;
-                    console.log(offsetLeftForBean, offsetTopForBean);
-
-                    var position = {
-                        "position" : "fixed",
-                        "top" : offsetTopForBean,
-                        "left" : offsetLeftForBean
-                    }
-                    coffeeBean.css(position).show();
+    // super power animation triggered on both buttons events :)
+    function showNextElement(){
+        sections.eq(counter).show();
+        var currentArticle = sections.eq(counter).find('article');
+        counter++;
+        container.animate({top: -distance*(counter)}, 800, function(){
+            currentArticle.find('h2').fadeIn(1000, function(){
+                currentArticle.find('div').fadeIn(1000, function(){
+                    nextButton.appendTo(currentArticle).fadeIn(1000);
                 });
             });
-        });
 
-    });
-
-    // animation showing next sections
-    function showNextElement(currentElement){
-        var parentArticle = currentElement.closest('article');
-        var parentSection = parentArticle.parent();
-        var nextSection = parentSection.next();
-        var nextArticle = nextSection.find('article');
-        var distance = nextSection.height();
-
-        nextSection.show();
-        nextArticle.show();
-        
-        currentElement.detach(); // detach 'next' button to use it in next section
-        $('main').animate({scrollTop: distance}, 800, function(){
-            parentSection.hide();
-            nextArticle.find('h2').fadeIn(1000, function(){
-                nextArticle.find('div').fadeIn(1000, function(){
-                    currentElement.appendTo(nextArticle).fadeIn(1000);
-                    
-                });
-            });
-            
         });
     }
 
+    // initial scroll animation: header button event
+    headerButton.on('click', function(){
+        $('main').addClass('main-flex');
+        showNextElement();
+    });
+
+    // next button event
     nextButton.on('click', function(){
-        showNextElement($(this));
+        nextButton.detach();
+        showNextElement();
     });
 
 });
